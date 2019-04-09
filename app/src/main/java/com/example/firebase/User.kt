4c9298eebcc0fde1cb601b9c37a6f4e2a0cf2a8c
Abhijeet <abhijeetprabhu15@gmail.com>
2019-04-08 17:58:04 -0700

    Laid groundwork for creating and adding new user to database.
    Signed-off-by: Abhijeet <abhijeetprabhu15@gmail.com>

---
+++ app/src/main/java/com/example/firebase/User.kt
@@ -0,0 +1,14 @@
+package com.example.firebase
+
+class User(
+    var userID: String,
+    var userEmailAddress: String,
+    var userPassword: String,
+    var userNickName: String
+) {
+
+    var userBio: String = ""
+    var profilePic: String = ""
+
+
+}
